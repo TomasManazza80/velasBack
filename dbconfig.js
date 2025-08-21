97304aa574cd126d25c682a80277c6ad06c813f6
TomasManazza80 <tomas.manazza19@gmail.com>
2025-08-21 14:52:56 -0300

    dbAded

--- dbconfig.js
+++ dbconfig.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   "db": {
-    "database": process.env.DATABASE,
+    "database": process.env.NAME,
     "username": process.env.USERNAME,
     "password": process.env.PASSWORD,
     "host": process.env.HOST,
